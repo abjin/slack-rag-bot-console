351b7d143119ac2c206dc9d3750481f911abb9f2
abjin <abjin69@gmail.com>
2025-12-23 15:17:06 +0900

    migrate mysql to postgresql

--- prisma/migrations/20250604133417_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[tenantId]` on the table `Tenants` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Tenants_tenantId_key` ON `Tenants`(`tenantId`);
